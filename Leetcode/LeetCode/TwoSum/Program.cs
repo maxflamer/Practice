a0c76336fa364a32f7168c6c1fc3e27b9cf9238e
maxflamer <km_95@hotmail.com>
2019-02-12 13:29:19 +0530

    Added TwoSum project

---
+++ Leetcode/LeetCode/TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 12, 3, 5 };
+            int target = 9;
+            int i, j;
+            int[] answer = new int[2];
+            for (i = 0; i < nums.Length; i++)
+            {
+                for (j = 1; j < nums.Length && j != i; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        answer[0] = i;
+                        answer[1] = j;
+                    }
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
