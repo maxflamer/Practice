112f8a424afb14e2eade9f017f5b4ad8b450769a
maxflamer <km_95@hotmail.com>
2019-07-23 18:41:07 +0530

    Added SearchInsertPosition

---
+++ Leetcode/SearchInsertPosition/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInsertPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 3, 5, 6 };
+            int target = 7;
+
+            if (nums[0] > target)
+                Console.WriteLine("0");
+            else if (nums[nums.Length - 1] == target)
+                Console.WriteLine(nums.Length - 1);
+            else if (nums[nums.Length - 1] < target)
+                Console.WriteLine(nums.Length);
+            else
+            {
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    if (nums[i] == target)
+                        Console.WriteLine(i);
+                    else if (nums[i] < target && nums[i + 1] > target)
+                        Console.WriteLine(i + 1);
+                }
+            }
+        }
+    }
+}
